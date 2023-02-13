3f35cd3ed7a1e7a683cddf196599a4edc42472dd
Lavino Menezes <lprogramacao@protonmail.com>
2023-02-13 20:47:38 -0300

    first

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='data_analisys_challenge_from_ifood',
+    author='lavino_menezes',
+    license='',
+)
